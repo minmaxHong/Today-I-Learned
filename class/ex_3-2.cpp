36c79d3652bef9445b974d07c716ab00bb13fc08
minmaxHong <landsky1234@naver.com>
2024-09-04 03:25:00 +0900

    docs: 9/3 repo

---
+++ class/ex_3-2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class Rectangle {
+public:
+	int width;
+	int height;
+	int getArea();
+};
+
+int Rectangle::getArea() {
+	return width * height;
+}
+
+int main() {
+	Rectangle rect;
+	rect.width = 3;
+	rect.height = 5;
+	cout << "사각형 면적은 " << rect.getArea() << "\n";
+}
